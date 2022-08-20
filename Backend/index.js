7cd2d3c3f33e5067748c4b67533d8e578d516163
SRIHARI <sshsrn@gmail.com>
2022-08-20 17:44:49 +0530

    updated backend

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 const cors = require("cors");
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = 8000;
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
